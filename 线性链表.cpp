888e5092d6e3326b43b15f0185793045d367b324
yuruotong <yuruotong1@bupt.edu.cn>
2016-10-11 17:55:39 +0800

    完成添加和删除的操作

--- 线性链表.cpp
+++ 线性链表.cpp
@@ -1,3 +1,4 @@
+#include"malloc.h"
 typedef struct LNode{
 	int data;
 	LNode* next;
@@ -15,9 +16,39 @@ typedef struct LNode{
  } 
  bool ListInsert_L(LinkList &L,int i,int e){
  	LinkList p=L;
- 	int j=0;
- 	
- }
+    int j=0;
+    while(p&&j<i-1){
+    	p=p->next;
+    	++j;
+    }
+    if(!p||j>i-1) return false;
+    //--------------------------------------------------------------------
+    //以下是重点 
+    LinkList s=(LinkList)malloc(sizeof(LNode)); //添加新的节点S 
+    s->data=e;
+    s->next=p->next;
+	p->next=s;
+	//--------------------------------------------------------------------- 
+	return true; 
+    
+}
+bool ListDelete_L(LinkList &L,int i,int &e){
+	LinkList p=L;
+    int j=0;
+    while(p->next&&j<i-1){
+    	p=p->next;
+    	++j;
+    }
+    if(!p->next||j>i-1) return false;
+    //-------------------------------------------------------
+	//下面的顺序不能随意修改，一定理清转换关系 
+	LinkList q=p->next;
+    p->next=q->next;
+	e=q->data;
+	free(q);  //注意回收 
+	//------------------------------------------------------------- 
+ 	return true;
+}
 
 int main(){
 	return 0;
