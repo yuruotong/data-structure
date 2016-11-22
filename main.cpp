e63d8fae638beb2d8cac01355b4d6bc84eeee806
yuruotong <yuruotong1@bupt.edu.cn>
2016-11-22 20:41:37 +0800

    树

--- main.cpp
+++ main.cpp
@@ -1,63 +1,94 @@
-#include"stdio.h"
-#include"malloc.h"
+#include<stdio.h>
 #include"string.h"
+#include<malloc.h>
 #include"stdlib.h"
-typedef struct{
-	char *ch;
-	int length;
-}HString;
-void InsertString(HString*T,char in_put[]){
-	if(!(T->ch=(char*)malloc(100*sizeof(char))))printf("error");
-	for(int i=1;i<=strlen(in_put);i++){
-		T->ch[i]=in_put[i];
-		T->length=i;
+struct pTree{
+char data;
+pTree *pLChild;
+pTree *pRChild;
+
+};
+void creat(struct pTree*&TT){
+	char in_put;
+	scanf_s("%c",&in_put,2);
+
+	if (in_put == '#') TT = NULL;
+		else{
+		if(!(TT=(struct pTree*)malloc(sizeof(pTree)))) printf("分配失败");
+		TT->data=in_put;
+		creat(TT->pLChild);
+		creat(TT->pRChild);
+		
 	
 	}
 
-}
-void getnext(HString *T,int next[]){
-int i=1;
-next[1]=0;
-int j=0;
-while(i<T->ch[0]){
-	if(j==0||T->ch[i]==T->ch[j]){
-		++i;
-		++j;
-		if(T->ch[i]!=T->ch[j]) next[i]=j;
-		else next[i]=next[j];
 	
 	}
-	else j=next[j];
-}
-} 
-int pipei(HString *S,HString *T,int &pos,int next[]){
-	int i = pos;
-	int j=1;
-	while(i<=S->ch[0]&&j<=T->ch[0]){
-		if(j==0||S->ch[i]==T->ch[j]){i++;j++;}
-		else j=next[j];
+   
+
+
+
+void xianxu(struct pTree* T){
+	if(NULL!=T){
+	printf("%c",T->data);
+	if(NULL!=T->pLChild)
+	xianxu(T->pLChild);
+	if(NULL!=T->pRChild)
+	xianxu(T->pRChild);
 	}
-	if(j>T->ch[0])return i-T->ch[0];
-	else return 0;
 
 }
+void zhongxu(struct pTree* T){
+	if(NULL!=T){
+	
+	if(NULL!=T->pLChild)
+	zhongxu(T->pLChild);
+	printf("%c",T->data);
+	if(NULL!=T->pRChild)
+	zhongxu(T->pRChild);
+	}
+
+}
+void houxu(struct pTree* T){
+	if(NULL!=T){
+	if(NULL!=T->pLChild)
+	houxu(T->pLChild);
+	
+	if(NULL!=T->pRChild)
+	houxu(T->pRChild);
+	printf("%c",T->data);
+	}
+
+}
+
+void print(struct pTree* T, int t) {
+
+	
+	if (NULL!=T) {
+
+		print(T->pRChild, t + 5);
+		int i;
+		for (i = 0; i < t; i++) printf(" ");
+		printf("%c\n", T->data);
+		print(T->pLChild, t + 5);
+	}
+
+}
+
 int main(){
-int pos;
-int next2[100];
-HString *T1=(HString*)malloc(sizeof(HString));
-HString *T2=(HString*)malloc(sizeof(HString));
-InsertString(T1,"adfadsfadf");
-InsertString(T2,"adfad");
-getnext(T2,next2);
-pipei(T1,T2,pos,next2);
- //---------------------------
+printf("请输入树（先序）\n");
 
-//---------------------------
-printf("%d",pos);
+struct pTree* TT=(struct pTree*)malloc(sizeof(pTree)); //用于保存上一个树的地址
+
+creat(TT);
+//printf("先序");
+//xianxu(TT);
+//printf("中序");
+//zhongxu(TT);
+//printf("后序");
+//houxu(TT);
+
+print(TT,5);
 system("pause");
 
-
-
-
-return 0;
-}
+return 0;}
